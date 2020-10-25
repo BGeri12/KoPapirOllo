57177d987682b00ababcef838764e544878c55cc
BGeri12 <gerib2001@gmail.com>
2020-10-25 20:45:54 +0100

    Fejlesztés életcsokkentesek megírasa

--- app/src/main/java/com/example/kopapirollo/MainActivity.java
+++ app/src/main/java/com/example/kopapirollo/MainActivity.java
@@ -14,10 +14,12 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     private ImageView imagekepTe, imagekepGep;
-    private TextView ember, gep;
+    private ImageView gimageHp1, gimageHp2, gimageHp3;
+    private ImageView eimageHp1, eimageHp2, eimageHp3;
+    private TextView emberkiir, gepkiir, dontetlen;
     private Button btnKo, btnPapir, btnOllo;
     private String gepValasztas, emberValasztas;
-    private int gepSzamol,emberSzamol,dontetlenSzamol;
+    private int gepSzamol,emberSzamol,dontetlenSzamol,elet;
     Random rnd;
 
     @Override
@@ -27,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
         rnd = new Random();
         init();
+        elet = 3;
 
 
 
@@ -74,43 +77,97 @@ public class MainActivity extends AppCompatActivity {
 
     if (emberValasztas.equals("Kő") && gepValasztas.equals("Papir")){
         Toast.makeText(MainActivity.this, "Ön vesztett, a gép nyert", Toast.LENGTH_SHORT).show();
-        gep++;
+        gepSzamol++;
+        gepkiir.setText(String.valueOf(gepSzamol));
+        eletCsokkentesEmber();
     }else
     if (emberValasztas.equals("Kő") && gepValasztas.equals("Olló")){
             Toast.makeText(MainActivity.this, "Ön nyert, a gép vesztett", Toast.LENGTH_SHORT).show();
             emberSzamol++;
+            emberkiir.setText(String.valueOf(emberSzamol));
+            eletCsokkentesGep();
         }else
     if (emberValasztas.equals("Papir") && gepValasztas.equals("Kő")){
             Toast.makeText(MainActivity.this, "Ön nyert, a gép vesztett", Toast.LENGTH_SHORT).show();
             emberSzamol++;
+            emberkiir.setText(String.valueOf(emberSzamol));
+            eletCsokkentesGep();
         }else
     if (emberValasztas.equals("Papir") && gepValasztas.equals("Olló")){
             Toast.makeText(MainActivity.this, "Ön vesztett, a gép nyert", Toast.LENGTH_SHORT).show();
             gepSzamol++;
+            gepkiir.setText(String.valueOf(gepSzamol));
+            eletCsokkentesEmber();
         }else
     if (emberValasztas.equals("Olló") && gepValasztas.equals("Papir")){
             Toast.makeText(MainActivity.this, "Ön nyert, a gép vesztett", Toast.LENGTH_SHORT).show();
             emberSzamol++;
+            emberkiir.setText(String.valueOf(emberSzamol));
+            eletCsokkentesGep();
         }else
     if (emberValasztas.equals("Olló") && gepValasztas.equals("Kő")){
             Toast.makeText(MainActivity.this, "Ön vesztett, a gép nyert", Toast.LENGTH_SHORT).show();
             gepSzamol++;
+            gepkiir.setText(String.valueOf(gepSzamol));
+            eletCsokkentesEmber();
         }else
     if (emberValasztas.equals("Olló") && gepValasztas.equals("Olló")){
             Toast.makeText(MainActivity.this, "Ez döntetlen", Toast.LENGTH_SHORT).show();
             dontetlenSzamol++;
+            dontetlen.setText(String.valueOf(dontetlenSzamol));
         }else
     if (emberValasztas.equals("Kő") && gepValasztas.equals("Kő")){
             Toast.makeText(MainActivity.this, "Ez döntetlen", Toast.LENGTH_SHORT).show();
             dontetlenSzamol++;
+            dontetlen.setText(String.valueOf(dontetlenSzamol));
         }else
     if (emberValasztas.equals("Papir") && gepValasztas.equals("Papir")){
             Toast.makeText(MainActivity.this, "Ez döntetlen", Toast.LENGTH_SHORT).show();
             dontetlenSzamol++;
+            dontetlen.setText(String.valueOf(dontetlenSzamol));
         }
 
     }
 
+    private void eletCsokkentesGep() {
+        switch (elet){
+            case 3:
+                gimageHp1.setImageResource(R.drawable.heart1);
+                break;
+            case 2:
+                gimageHp2.setImageResource(R.drawable.heart1);
+                break;
+            case 1:
+                gimageHp3.setImageResource(R.drawable.heart1);
+                break;
+            default:
+                break;
+        }
+        elet--;
+        if (elet < 1 || gepSzamol >= 3){
+
+        }
+    }
+
+    private void eletCsokkentesEmber() {
+        switch (elet){
+            case 3:
+                eimageHp1.setImageResource(R.drawable.heart1);
+                break;
+            case 2:
+                eimageHp2.setImageResource(R.drawable.heart1);
+                break;
+            case 1:
+                eimageHp3.setImageResource(R.drawable.heart1);
+                break;
+            default:
+                break;
+        }
+        elet--;
+        if (elet < 1 || emberSzamol >= 3){
+
+        }
+    }
 
     private void init() {
 
@@ -119,9 +176,15 @@ public class MainActivity extends AppCompatActivity {
         btnKo = findViewById(R.id.btn_Ko);
         btnPapir = findViewById(R.id.btn_Papir);
         btnOllo = findViewById(R.id.btn_Ollo);
-        ember = findViewById(R.id.ember);
-        gep = findViewById(R.id.gep);
-
+        emberkiir = findViewById(R.id.ember);
+        gepkiir = findViewById(R.id.gep);
+        gimageHp1 = findViewById(R.id.image_ghp1);
+        gimageHp2 = findViewById(R.id.image_ghp2);
+        gimageHp3 = findViewById(R.id.image_ghp3);
+        eimageHp1 = findViewById(R.id.image_ehp1);
+        eimageHp2 = findViewById(R.id.image_ehp2);
+        eimageHp3 = findViewById(R.id.image_ehp3);
+        dontetlen = findViewById(R.id.dontetlen);
     }
 
 
